da9ae471a83becb99988910619d58db15c4e25b0
Ryan O'Connor <ryan.oconnor@gmail.com>
2021-12-23 11:01:51 -0500

    Added a linechart as well and cleaned up the UI

--- src/App.js
+++ src/App.js
@@ -1,20 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
 import { createSearchJob, getData } from '@splunk/splunk-utils/search';
 import { useState, useEffect } from 'react'
 import SingleValue from '@splunk/visualizations/SingleValue';
+import Line from '@splunk/visualizations/Line';
+import ColumnLayout from '@splunk/react-ui/ColumnLayout';
+
+import Link from '@splunk/react-ui/Link'
+import List from '@splunk/react-ui/List'
+import P from '@splunk/react-ui/Paragraph';
+import Heading from '@splunk/react-ui/Heading';
+import { useSplunkTheme } from '@splunk/themes';
 
 function App() {
 
-  const [sid, setSid] = useState()
-  const [searchResultsFields, setSearchResultsFields] = useState()
-  const [searchResultsColumns, setSearchResultsColumns] = useState()
-  const [secondsToComplete, setSecondsToComplete] = useState()
+
+  const [lineSid, setLineSid] = useState()
+
+
+  const splunkSearchLine = "search index=_internal | timechart span=10s count"
+
+  const [lineSearchResultsFields, setLineSearchResultsFields] = useState()
+  const [lineSearchResultsColumns, setLineSearchResultsColumns] = useState()
+  const [lineSecondsToComplete, setLineSecondsToComplete] = useState()
+
+
+  const [singleValueSid, setSingleValueSid] = useState()
+  const splunkSearchSingleValue = "search index=_internal | stats count"
+  const [singleValueSearchResultsFields, setSingleValueSearchResultsFields] = useState()
+  const [singleValueSearchResultsColumns, setSingleValueSearchResultsColumns] = useState()
+  const [singleValueSeondsToComplete, setSingleValueSecondsToComplete] = useState()
 
 
   const sessionKey = "eyJraWQiOiJzcGx1bmsuc2VjcmV0IiwiYWxnIjoiSFM1MTIiLCJ2ZXIiOiJ2MiIsInR0eXAiOiJzdGF0aWMifQ.eyJpc3MiOiJhZG1pbiBmcm9tIEMwMldSMUg0SFRERCIsInN1YiI6ImFkbWluIiwiYXVkIjoiVGVzdGluZyIsImlkcCI6IlNwbHVuayIsImp0aSI6IjY4ZTcwYjA5MDljYTA3ZmI2ZDAwZTVkMTE4MTNkZDIwMDk3ZTY0ZjhlYjU2ZGFjZjJlNzcyZGFlMDlhNzc3NmIiLCJpYXQiOjE2NDAyMDc3NzgsImV4cCI6MTY0Mjc5OTc3OCwibmJyIjoxNjQwMjA3Nzc4fQ.Ru3FNEbPE_c1OSmM3NBzHHSdlu2FsXhvRppqsfLX8GsNpViPtsoGd53CUcNb_thIVcgPN29lMkAz_BhaxXRQCg"
   const splunkURL = "https://localhost:8089"
-  const splunkSearch = "search index=_internal | stats count"
+
 
   const headers = {
     headers: {
@@ -26,26 +45,35 @@ function App() {
 
   useEffect(() => {
 
-    if (!sid) {
-      createJob()
+    if (!singleValueSid) {
+      createJob(splunkSearchSingleValue)
         .then(data => data)
         .then(sidJob => {
-          setSid(sidJob)
-          load(sidJob)
+          setSingleValueSid(sidJob)
+          load(sidJob, setSingleValueSecondsToComplete, setSingleValueSearchResultsFields, setSingleValueSearchResultsColumns)
+        })
+    }
+
+    if (!lineSid) {
+      createJob(splunkSearchLine)
+        .then(data => data)
+        .then(sidJob => {
+          setLineSid(sidJob)
+          load(sidJob, setLineSecondsToComplete, setLineSearchResultsFields, setLineSearchResultsColumns)
         })
     }
   }, [])
 
-  async function load(sidJob) { 
+  async function load(sidJob, completeFunc, fieldsFunc, columnsFunc) {
     var completeSeconds = 0
 
     for (var i = 0; i < 7; i++) {
-      fetchData(sidJob)
+      fetchData(sidJob, fieldsFunc, columnsFunc)
         .then(data => data)
         .then(sidJob => {
           if (sidJob) {
             completeSeconds = completeSeconds + 1
-            setSecondsToComplete(completeSeconds)
+            completeFunc(completeSeconds)
           }
         })
       if (!completeSeconds) {
@@ -57,9 +85,9 @@ function App() {
     }
   }
 
-  const createJob = async () => {
+  const createJob = async (search) => {
     const n = createSearchJob({
-      search: splunkSearch,
+      search: search,
       earliest_time: '-60m@m',
       latest_time: 'now',
     }, {}, { splunkdPath: splunkURL, app: "search", owner: "admin" }, headers)
@@ -69,13 +97,13 @@ function App() {
     return n
   };
 
-  const fetchData = async (sidJob) => {
+  const fetchData = async (sidJob, fieldsFunc, columnsFunc) => {
     const n = await getData(sidJob, "results", { output_mode: "json_cols" }, { splunkdPath: splunkURL, app: "search", owner: "admin" }, headers)
       .then(response => response)
       .then(data => {
         if (data) {
-          setSearchResultsFields(data.fields)
-          setSearchResultsColumns(data.columns)
+          fieldsFunc(data.fields)
+          columnsFunc(data.columns)
           return data
         }
       })
@@ -85,40 +113,92 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>@splunk/splunk-utils Example app.</h1> 
-        <p>This app uses couple of packages available on https://splunkui.splunk.com (@splunk/splunk-utils, @splunk/visualizations)</p>
-          <p>Be mindful that you'll need to setup your own Auth Token (https://docs.splunk.com/Documentation/Splunk/8.2.3/Security/CreateAuthTokens) to use this app.  </p>
-        
-      <SingleValue
-          options={{
-            majorColor: '#008000',
-          }}
-          dataSources={{
+        <Heading level={1}>@splunk/splunk-utils Example app</Heading>
+        <P>This app will show you how to query Splunk from a remote webapp using our Splunk UI Toolkit in React. It uses a couple of packages listed below: </P>
+        <List>
+          <List.Item><Link to="https://www.npmjs.com/package/@splunk/splunk-utils">@splunk/splunk-utils</Link></List.Item>
+          <ul><li><Link to="https://splunkui.splunkeng.com/Packages/splunk-utils">Documentation</Link></li></ul>
+          <List.Item><Link to="https://www.npmjs.com/package/@splunk/visualizations">@splunk/visualizations</Link></List.Item>
+          <ul><li><Link to="https://splunkui.splunkeng.com/Packages/visualizations">Documentation</Link></li></ul>
+        </List>
+
+        <Heading level={2}>Setup Instructions</Heading>
+        <P>You'll need to take complete a couple of setup steps for this app to work with your Splunk Environment.</P>
+        <List>
+          <List.Item>Setup your own Authentication Token to use this app. Instructions can be found <Link to="https://docs.splunk.com/Documentation/Splunk/8.2.3/Security/CreateAuthTokens">here</Link></List.Item>
+          <List.Item>You'll need to configure CORS. Instructions can be found <Link to="https://dev.splunk.com/enterprise/docs/developapps/visualizedata/usesplunkjsstack/communicatesplunkserver/">here</Link></List.Item>
+        </List>
+
+
+<div style={{width:"100%"}}>
+  <div style={{float:"left"}}>
+              <SingleValue
+                options={{
+                  majorColor: '#008000',
+                }}
+                dataSources={{
+                  primary: {
+                    data: {
+                      columns: singleValueSearchResultsColumns,
+                      fields: singleValueSearchResultsFields
+                    },
+                    meta: {},
+                  },
+                }}
+              />
+
+              <Heading level={2}>This is a Single Value that represents the following search: </Heading>
+              <P>Search: {splunkSearchSingleValue}</P>
+              <P>
+
+                {"Splunk SID: " + singleValueSid}
+              </P>
+              <P>
+                {"Seconds to Complete: " + JSON.stringify(singleValueSeondsToComplete)}
+              </P>
+              <P>
+                {"Splunk Results - Fields: " + JSON.stringify(singleValueSearchResultsFields)}
+              </P>
+              <P>
+                {"Splunk Results - Columns: " + JSON.stringify(singleValueSearchResultsColumns)}
+              </P>
+      </div>
+      <div style={{float:"right", width: "50%"
+}}>
+
+            
+              <Line
+        options={{}}
+        dataSources={{
             primary: {
-              data: {
-                columns: searchResultsColumns,
-                fields: searchResultsFields
-              },
-              meta: {},
+                
+                data: {
+                    fields: lineSearchResultsFields,
+                    columns: lineSearchResultsColumns,
+                },
+                meta: {  },
             },
-          }}
-        />        
-        <h3>Info about this search</h3>
-        
-        <h4>This is a Single Value that represents the following search: </h4>
-        <p>Search: {splunkSearch}</p><p>
-          
-          {"Splunk SID: " + sid}
-        </p>
-        <p>
-          {"Seconds to Complete: " + JSON.stringify(secondsToComplete)}
-        </p>
-        <p>
-          {"Splunk Results - Fields: " + JSON.stringify(searchResultsFields)}
-        </p>
-        <p>
-          {"Splunk Results - Columns: " + JSON.stringify(searchResultsColumns)}
-        </p>
+        }}
+    />
+              <Heading level={2}>This is a Line Chart that represents the following search: </Heading>
+
+              <P>Search: {splunkSearchLine}</P>
+              <P>
+
+                {"Splunk SID: " + lineSid}
+              </P>
+              <P>
+                {"Seconds to Complete: " + JSON.stringify(lineSecondsToComplete)}
+              </P>
+              <P>
+                {"Splunk Results - Fields: " + JSON.stringify(lineSearchResultsFields)}
+              </P>
+              <P>
+                {"Splunk Results - Columns: " + JSON.stringify(lineSearchResultsColumns)}
+              </P>
+    </div>
+    </div>
+
       </header>
     </div>
   );
