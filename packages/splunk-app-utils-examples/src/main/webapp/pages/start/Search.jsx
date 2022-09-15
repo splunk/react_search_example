d743198043b420858dea8d663d55ef2c2a5aefaf
Ryan O'Connor <ryan.oconnor@gmail.com>
2022-09-15 16:07:17 -0400

    Adding example for cached search

--- packages/splunk-app-utils-examples/src/main/webapp/pages/start/Search.jsx
+++ packages/splunk-app-utils-examples/src/main/webapp/pages/start/Search.jsx
@@ -10,6 +10,7 @@ import {
     getCachedSearch,
     stripLeadingSearchCommand,
     addLeadingSearchCommand,
+    createJava5HashForString,
 } from '@splunk/splunk-utils/search';
 import JSONTree from '@splunk/react-ui/JSONTree';
 import P from '@splunk/react-ui/Paragraph';
@@ -19,11 +20,15 @@ function Search() {
     const [createSearchJobSid, setCreateSearchJobSid] = useState('');
     const [createSecondSearchJobSid, setCreateSecondSearchJobSid] = useState('');
 
+    const [oneShotResults, setOneShotResults] = useState({});
+
+    const [cachedSearchResults, setCachedSearchResults] = useState({});
+
     const [searchBool, setSearchingBool] = useState(false);
     const [searchResultsFields, setSearchResultsFields] = useState();
     const [searchResultsColumns, setSearchResultsColumns] = useState();
     const [secondsToComplete, setSecondsToComplete] = useState(0);
-    const [batchSearchResults, setBatchSearchResults] = useState();
+    const [batchSearchResults, setBatchSearchResults] = useState({ entry: [] });
 
     const timer = (ms) => new Promise((res) => setTimeout(res, ms));
 
@@ -46,8 +51,9 @@ function Search() {
                 }
             });
 
+        //This is just a Secondary Search we use for the batchGetSearches example
         createSearchJob({
-            search: 'search index=_internal | stats count by sourcetype',
+            search: 'search index=_internal | stats count by sourcetype | eval search="Second Search"',
             earliest_time: '-1h',
             latest_time: 'now',
         })
@@ -56,6 +62,29 @@ function Search() {
                 if (data.sid != '') {
                     setCreateSecondSearchJobSid(data.sid);
                 }
+            });
+
+        createSearchJob({
+            search: 'search index=_internal | stats count by sourcetype | eval search="Cached Search"',
+            earliest_time: '-15m',
+            latest_time: 'now',
+            label: 'Cached Search Example',
+        })
+            .then((response) => response)
+            .then((data) => {
+                if (data.sid != '') {
+                    setCreateSecondSearchJobSid(data.sid);
+                }
+            });
+
+        oneShot({
+            search: 'search index=_internal | stats count by sourcetype | eval search="One Shot results"',
+            earliest_time: '-1h',
+            latest_time: 'now',
+        })
+            .then((response) => response)
+            .then((data) => {
+                setOneShotResults(data);
             });
     }, []);
 
@@ -66,6 +95,21 @@ function Search() {
             });
         }
     }, [createSearchJobSid, createSecondSearchJobSid]);
+
+    function getCachedSearchResults() {
+        getCachedSearch(
+            {
+                search: 'search index=_internal | stats count by sourcetype | eval search="Cached Search"',
+                earliest_time: '-15m',
+                latest_time: 'now',
+                label: 'Cached Search Example',
+            },
+            { cacheLimit: 0 }
+        ).then((searchState) => {
+            setCachedSearchResults(searchState);
+            console.log(searchState);
+        });
+    }
 
     useEffect(() => {
         if (createSearchJobSid != '') {
@@ -120,39 +164,62 @@ function Search() {
         return n;
     };
 
+    async function extendTTL() {
+        postAction(createSearchJobSid, { action: 'save' }).then((response) => {
+            batchGetSearches([createSearchJobSid, createSecondSearchJobSid]).then((data) => {
+                setBatchSearchResults(data);
+            });
+        });
+        postAction(createSecondSearchJobSid, { action: 'save' }).then((response) => {
+            batchGetSearches([createSearchJobSid, createSecondSearchJobSid]).then((data) => {
+                setBatchSearchResults(data);
+            });
+        });
+    }
+
     return (
         <div style={{ vericalAlign: 'top' }}>
             <Heading level={1}>Search</Heading>
             <hr />
             <Heading level={3}>Add a Leading Search Command</Heading>
-
             <P>{addLeadingSearchCommand('index=_internal')}</P>
-
             <Heading level={3}>Validate a Splunk Search</Heading>
-
             <JSONTree json={getValidResults}></JSONTree>
-
+            <Heading level={3}>Create Java Hash for String</Heading>
+            <P>{createJava5HashForString('index=_internal')}</P>
             <Heading level={3}>Strip a Leading Search Command</Heading>
-
             <P>{stripLeadingSearchCommand(search)}</P>
-
             <Heading level={3}>Create a Search Job</Heading>
-
             <P>Sid: {createSearchJobSid}</P>
-
             <Heading level={3}>Get Results from Search Job</Heading>
-
             <P>Search Job Is Done: {String(searchBool)}</P>
             <P>Search Job Timer: {secondsToComplete}</P>
             <P>Fields: {searchResultsFields}</P>
             <P>Columns: {searchResultsColumns}</P>
-
             <Heading level={3}>Get Results from Multiple Search Jobs (batchGetSearches)</Heading>
-
             <P>Sid #1: {String(createSearchJobSid)}</P>
             <P>Sid #2: {String(createSecondSearchJobSid)}</P>
-
             <JSONTree json={batchSearchResults}></JSONTree>
+            <P>Sid #1: {String(createSearchJobSid)}</P>
+            <P>Sid #2: {String(createSecondSearchJobSid)}</P>
+            <Heading level={3}>One Shot</Heading>
+            <JSONTree json={oneShotResults}></JSONTree>
+            <Heading level={3}>
+                Extend TTL for Search Jobs using postAction:{createSecondSearchJobSid}{' '}
+            </Heading>
+            {batchSearchResults.entry.map((key, value) => {
+                return (
+                    <>
+                        <P>SID: {String(key.content.sid)}</P>
+                        <P>TTL: {String(key.content.ttl)}</P>
+                    </>
+                );
+            })}
+            <button onClick={(e) => extendTTL(e)}>Extend TTL for Search Jobs</button>
+
+            <Heading level={3}>Cached Search Results</Heading>
+            <JSONTree json={cachedSearchResults}></JSONTree>
+            <button onClick={(e) => getCachedSearchResults(e)}>Get Cached Searches</button>
         </div>
     );
 }
